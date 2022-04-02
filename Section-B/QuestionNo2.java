bab21502481096757c96cc1245a479bdbea2fca6
Rk-7248-github <102796442+Rk-7248-github@users.noreply.github.com>
2022-04-02 13:33:54 +0530

    Create QuestionNo2.java

---
+++ Section-B/QuestionNo2.java
@@ -0,0 +1,218 @@
+import java.util.*;  
+public class QuestionNo2{
+
+// this method is used to calculating the totle steps adventurer moving to reach gold.
+static int[] calculateSteps(int adventurer_current, int adventurer, int monster_current, int monster, int gold_current, int gold){
+       
+       int count = 0;
+       
+          if(adventurer_current < gold_current){
+            
+            //this for loop using to move the adventurer either RIGHT or DOWN
+             for(int a = adventurer_current+1; a <= gold_current; a++){
+                adventurer_current = a;
+                count++;
+                
+                if(adventurer_current == gold_current && adventurer == gold){
+                     int[] temp = {1, count};
+                      return temp;
+                }
+                
+               //this nested if condition using to move the monster 
+                if(monster_current < gold_current){
+                    monster_current++;
+                     if(monster_current == gold_current && monster == gold){
+                        
+                        int[] temp = {0};
+                        return temp;
+                    }
+                    
+                }else if(monster_current > gold_current){
+                    monster_current--;
+                     if(monster_current == gold_current && monster == gold){
+                        
+                        int[] temp = {0};
+                        return temp;
+                    }
+                }else{
+                    if(monster_current == gold_current && monster == gold){
+                        int[] temp = {0};
+                        return temp;
+                    }
+                    
+                }
+            
+             }
+         
+        }else if(adventurer_current > gold_current){
+            
+             //this for loop using to move the adventurer either LEFT or UP
+             for(int a = adventurer_current-1; a >= gold_current; a--){
+                adventurer_current = a;
+                count++;
+                
+                if(adventurer_current == gold_current && adventurer == gold){
+                        int[] temp = {1, count};
+                        return temp;
+                }
+                
+               //this nested if condition using to move the monster
+                if(monster_current < gold_current){
+                    monster_current++;
+                     if(monster_current == gold_current && monster == gold){
+                        int[] temp = {0};
+                        return temp;
+                    }
+                    
+                }else if(monster_current > gold_current){
+                    monster_current--;
+                     if(monster_current == gold_current && monster == gold){
+                        int[] temp = {0};
+                        return temp;
+                    }
+                }else{
+                    if(monster_current == gold_current && monster == gold){
+                        int[] temp = {0};
+                        return temp;
+                    }
+                    
+                }
+             }
+         
+        }
+        
+        
+        int[] temp = {2, count, adventurer_current, monster_current};
+        
+        return temp;
+       
+   }
+   
+
+
+     public static void main(String []args){
+         
+
+        Scanner sc= new Scanner(System.in); 
+        System.out.print("Dimensions of the dungeon(Row x Column): ");
+        int rows = sc.nextInt();
+        int columns = sc.nextInt();
+       
+        //this if condition to check the given input is valied
+        if( rows < 1||  columns < 1){
+        
+          int[] temp = repeaterDimensions("Enter correct Dimensions of the dungeon(Row x Column): ");
+          rows = temp[0];
+          columns = temp[1];
+        }
+        System.out.println();
+        
+        System.out.print("Position of adventurer: ");
+        int adventurer_i = sc.nextInt();
+        int adventurer_j = sc.nextInt();
+       
+       //this if condition to check the given input is valied
+        if(adventurer_i > rows || adventurer_j > columns || adventurer_i < 1||  adventurer_j < 1){
+        
+          int[] temp = repeaterPosition(rows, columns,"Enter correct position of adventurer: ");
+          adventurer_i = temp[0];
+          adventurer_j = temp[1];
+        }
+        
+           System.out.print("Position of monster: ");
+        int m_i = sc.nextInt();
+        int m_j = sc.nextInt();
+       
+       //this if condition to check the given input is valied
+        if(m_i > rows || m_j > columns || m_i < 1||  m_j < 1){
+        
+          int[] temp = repeaterPosition(rows, columns,"Enter correct position of monster: ");
+          m_i = temp[0];
+          m_j = temp[1];
+        }
+         
+        
+        System.out.print("Position of gold: ");
+        int gold_i = sc.nextInt();
+        int gold_j = sc.nextInt();
+       
+       //this if condition to check the given input is valied
+        if(gold_i > rows || gold_j > columns || gold_i < 1||  gold_j < 1){
+        
+          int[] temp = repeaterPosition(rows, columns,"Enter correct position of gold: ");
+          gold_i = temp[0];
+          gold_j = temp[1];
+        }
+         
+       
+        int totleSteps = 0;
+        
+        
+        int[] iResult = calculateSteps(adventurer_j, adventurer_i, m_j, m_i, gold_j,gold_i);
+        
+        if(iResult[0] == 0){
+            System.out.print("  No possible solution");
+		return;
+        }else if(iResult[0] == 1){
+             totleSteps = iResult[1];
+             System.out.print("Minimum number of steps: " +totleSteps );
+		 return;
+        }else{
+            
+            totleSteps = iResult[1];
+            adventurer_j = iResult[2];
+            m_j = iResult[3];
+            
+        }
+     
+        
+        int[] jResult = calculateSteps(adventurer_i, adventurer_j, m_i, m_j, gold_i,gold_j);
+        
+         if(jResult[0] == 0){
+           
+            System.out.print("  No possible solution");
+        }else if(jResult[0] == 1){
+             totleSteps = totleSteps+jResult[1];
+             System.out.print("Minimum number of steps: " +totleSteps );
+        }
+
+            
+             
+    }
+    
+// this method is used to avoid taking invalied inputs
+       static int[] repeaterPosition(int rows, int columns, String info){
+       Scanner sc= new Scanner(System.in); 
+       System.out.print(info);
+       int i = sc.nextInt();
+       int j = sc.nextInt();
+       
+        if(i > rows || j > columns || rows < 1|| columns < 1){
+          int[] temp = repeaterPosition(rows, columns,info);
+          i = temp[0];
+          j = temp[1];
+        }
+       
+       int[] values = {i,j};
+       return values;
+   }
+   
+// this method is used to avoid taking invalied inputs
+    static int[] repeaterDimensions(String info){
+       Scanner sc= new Scanner(System.in); 
+       System.out.print(info);
+       int i = sc.nextInt();
+       int j = sc.nextInt();
+       
+        if(i < 1 || j < 1 ){
+          int[] temp = repeaterDimensions(info);
+          i = temp[0];
+          j = temp[1];
+        }
+       
+       int[] values = {i,j};
+       return values;
+   }
+  
+     
+}
